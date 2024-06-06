30a6bd80c48dfa814c20bf66b4c015d3eeffdc25
Brewen Couaran <45310490+brewcoua@users.noreply.github.com>
2024-06-06 09:36:25 +0200

    fix(prompt): remove all transformers for target prompt

--- src/cli/prompts/target.ts
+++ src/cli/prompts/target.ts
@@ -3,17 +3,8 @@ import { input } from '@inquirer/prompts'
 
 export async function promptTarget(prefilled?: string): Promise<string> {
     if (prefilled && prefilled.trim().length > 0) {
-        // Validate
         if (prefilled.match(VALID_TARGET_REGEX)) {
-            if (prefilled.match(/^https?:\/\//i)) {
-                return prefilled
-            }
-
-            if (prefilled.match(HTTPS_URL_REGEX)) {
-                return `https://${prefilled}`
-            }
-
-            return `http://${prefilled}`
+            return prefilled
         }
 
         throw new Error('Invalid URL, domain name, or IP address')
@@ -28,27 +19,7 @@ export async function promptTarget(prefilled?: string): Promise<string> {
 
             return 'Invalid URL, domain name, or IP address'
         },
-        transformer: (input: string) => {
-            // If it starts with http or https, return as is
-            if (input.match(/^https?:\/\//i)) {
-                return input
-            }
-
-            if (input.match(HTTPS_URL_REGEX)) {
-                return `https://${input}`
-            }
-
-            return `http://${input}`
-        },
     })
 
-    if (target.match(/^https?:\/\//i)) {
-        return target
-    }
-
-    if (target.match(HTTPS_URL_REGEX)) {
-        return `https://${target}`
-    }
-
-    return `http://${target}`
+    return target
 }
