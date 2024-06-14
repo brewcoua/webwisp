f16f2352f87c71e39dd4590588ec48b0dae47a6f
Brewen Couaran <45310490+brewcoua@users.noreply.github.com>
2024-06-14 10:05:20 +0200

    fix(lib): export runner status type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Agent } from './Agent'
 
 export { default as Runner } from './services/runner'
 export { default as ActionReport } from './services/runner/domain/ActionReport'
+export { default as RunnerStatus } from './services/runner/domain/RunnerStatus'
